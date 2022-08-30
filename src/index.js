1122504a8825040489d17856a2cbad4b9f038a63
allanzaparoli <allanzaparoli@gmail.com>
2022-08-30 11:27:44 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -12,8 +12,30 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// Meus código =>>>
+
+// Requisito 1
+
+const fs = require('fs').promises;
+
+async function locutor() {
+  const listaVazia = [];
+  try {
+    const data = await fs.readFile('src/talker.json', 'utf-8');
+    const filtroParse = JSON.parse(data);
+    return filtroParse;
+  } catch (err) {
+    return listaVazia;
+  }
+}
+
+app.get('/talker', async (req, res) => {
+  const lista = await locutor();
+  return res.status(200).json(lista);
+});
+
+// <<<= Fim dos Códigos.
+
 app.listen(PORT, () => {
   console.log('Online');
 });
-
-// iniciando o projeto
